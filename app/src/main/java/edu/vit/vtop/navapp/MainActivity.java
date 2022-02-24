7d8a4377b81e0837329a82eb946607e4e7e2c39e
Panshul Jindal <panshul.jindal2019@vitstudent.ac.in>
2022-02-24 11:22:17 +0530

    Maps Initialised

--- app/src/main/java/edu/vit/vtop/navapp/MainActivity.java
+++ app/src/main/java/edu/vit/vtop/navapp/MainActivity.java
@@ -2,6 +2,7 @@ package edu.vit.vtop.navapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(MainActivity.this,HomeActivity.class));
     }
 }
