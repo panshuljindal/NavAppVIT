26354802a1b3860436366f4b19edab4022996e74
Jatin7385 <jats7385@gmail.com>
2022-02-24 19:27:44 +0530

    Map Work commit

--- app/src/main/java/edu/vit/vtop/navapp/HomeActivity.java
+++ app/src/main/java/edu/vit/vtop/navapp/HomeActivity.java
@@ -1,15 +1,32 @@
 package edu.vit.vtop.navapp;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.os.Bundle;
+import android.content.Context;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 import edu.vit.vtop.navapp.databinding.ActivityHomeBinding;
 
@@ -17,6 +34,7 @@ public class HomeActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private ActivityHomeBinding binding;
+    private FloatingActionButton user_location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +47,96 @@ public class HomeActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        binding.userLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setUpMapIfNeeded();
+            }
+        });
     }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(getApplicationContext(), R.raw.map_style));
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Create a LatLngBounds that includes the VIT Campus bounds
+        LatLngBounds vitBounds = new LatLngBounds(
+                new LatLng(12.973208, 79.153890), // SW bounds
+                new LatLng(12.974411, 79.167821)  // NE bounds
+        );
+
+// Constrain the camera target to the VIT Campus bounds.
+        mMap.setLatLngBoundsForCameraTarget(vitBounds);
+
+
+//        ArrayList<LatLng> latLngList = new ArrayList<>();
+//        latLngList.add(new LatLng(-27.457, 153.040));
+//        latLngList.add(new LatLng(-33.852, 151.211));
+//        latLngList.add(new LatLng(-37.813, 144.962));
+//        latLngList.add(new LatLng(-34.928, 138.599));
+//        googleMap.addPolygon(PolygonOptions()
+//                .clickable(false)
+//                .addAll(latLngList)
+//                .fillColor(getColor(R.color.polygonColor))
+//                .strokeWidth(0f))
+
+        // Add a marker in VIT and move the camera
+        LatLng vit = new LatLng(12.974714, 79.164227);
+        mMap.addMarker(new MarkerOptions().position(vit).title("VIT")
+                .icon(BitmapFromVector(getApplicationContext(), R.drawable.ic_marker)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(vit, 15f));
+    }
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this::onMapReady);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+                    @Override
+                    public void onMyLocationChange(Location arg0) {
+                        mMap.addMarker(new MarkerOptions().position(new LatLng(arg0.getLatitude(), arg0.getLongitude())).title("It's Me!"));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(arg0.getLatitude(), arg0.getLongitude()), 15f));
+                    }
+                });
+            }
+        }
+    }
+
+    private BitmapDescriptor BitmapFromVector(Context context, int vectorResId) {
+        // below line is use to generate a drawable.
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+
+        // below line is use to set bounds to our vector drawable.
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+
+        // below line is use to create a bitmap for our
+        // drawable which we have added.
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+
+        // below line is use to add bitmap in our canvas.
+        Canvas canvas = new Canvas(bitmap);
+
+        // below line is use to draw our
+        // vector drawable in canvas.
+        vectorDrawable.draw(canvas);
+
+        // after generating our bitmap we are returning our bitmap.
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 }
