3c3ed80c6c63dbc10ec6d8d5d6be3442491c34e6
Haneet Arya <76744982+Haneet-Arya@users.noreply.github.com>
2022-03-21 23:08:29 +0530

    toast and location permission bug fixes

--- app/src/main/java/edu/vit/vtop/navapp/Activity/CategoryActivity.java
+++ app/src/main/java/edu/vit/vtop/navapp/Activity/CategoryActivity.java
@@ -1,12 +1,16 @@
 package edu.vit.vtop.navapp.Activity;
 
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.content.Intent;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -35,9 +39,33 @@ public class CategoryActivity extends AppCompatActivity {
     private ProgressBar progressBar;
     double ulat,ulng;
     boolean isUserLocationNull = true;
+    ActivityResultLauncher<String[]> locationPermissionRequest;
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+
+            locationPermissionRequest =
+                    registerForActivityResult(new ActivityResultContracts
+                                    .RequestMultiplePermissions(), result -> {
+                                Boolean fineLocationGranted = result.getOrDefault(
+                                        Manifest.permission.ACCESS_FINE_LOCATION, false);
+                                Boolean coarseLocationGranted = result.getOrDefault(
+                                        Manifest.permission.ACCESS_COARSE_LOCATION, false);
+                                if (fineLocationGranted != null && fineLocationGranted) {
+                                    // Precise location access granted.
+                                } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                                    // Only approximate location access granted.
+                                } else {
+                                    // No location access granted.
+                                }
+                            }
+                    );
+        }
+        catch (Exception e){
+
+        }
         setContentView(R.layout.activity_category);
 
         progressBar = findViewById(R.id.progressBar1);
@@ -60,7 +88,7 @@ public class CategoryActivity extends AppCompatActivity {
                     }
                     try {
                         list = response.body();
-                        PlacesAdapter adapter = new PlacesAdapter(list, CategoryActivity.this,ulat,ulng,isUserLocationNull);
+                        PlacesAdapter adapter = new PlacesAdapter(list, CategoryActivity.this,ulat,ulng,isUserLocationNull,locationPermissionRequest);
                         LinearLayoutManager manager = new LinearLayoutManager(CategoryActivity.this);
                         manager.setOrientation(RecyclerView.VERTICAL);
                         categories.setLayoutManager(manager);
